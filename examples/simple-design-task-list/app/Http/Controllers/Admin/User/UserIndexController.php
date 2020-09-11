81da8426daf27494e301fd03e54c9a44e1ecb316
田村行生 <>
2020-09-11 14:12:46 +0900

    update blade

--- examples/simple-design-task-list/app/Http/Controllers/Admin/User/UserIndexController.php
+++ examples/simple-design-task-list/app/Http/Controllers/Admin/User/UserIndexController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\Admin\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\UseCases\Admin\User\Interfaces\UserIndexInterface;
 
 class UserIndexController extends Controller
 {
-    public function __invoke()
+    public function __invoke(UserIndexInterface $useCase)
     {
+        $users = $useCase();
+
         return view('admin.user.index', [
-            'tasks' => [],
+            'users' => $users,
         ]);
     }
 }
