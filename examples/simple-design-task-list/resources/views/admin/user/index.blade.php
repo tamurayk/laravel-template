4e6a521dd0b68c32c0bbca9a30f5f29e61de802c
田村行生 <>
2020-09-18 18:32:22 +0900

    adjust design

--- examples/simple-design-task-list/resources/views/admin/user/index.blade.php
+++ examples/simple-design-task-list/resources/views/admin/user/index.blade.php
@@ -5,47 +5,64 @@
 
 @extends('layouts.admin')
 
+@section('header')
+  <div>
+    <h1>
+      <i class="fas fa-align-justify"></i> User
+      <a class="btn btn-success float-right" onclick="alert('not yet implemented.')"><i class="fas fa-plus"></i> Create</a>
+    </h1>
+  </div>
+@endsection
+
 @section('content')
-  <div class="container">
-    <div class="col-sm-offset-2 col-sm-8">
-      <div class="panel panel-default">
-        <div class="panel-body">
-          @include('common.errors')
+  <div class="col-sm-offset-2 col-sm-14">
+    <div class="panel panel-default">
+      <div class="panel-body">
+        @include('common.errors')
 
-          @if (count($paginator) > 0)
-          <div class="panel panel-default">
-            <div class="panel-heading">
-              Users
-            </div>
-
-            <div class="panel-body">
-              <table class="table table-striped task-table">
-                <thead>
-                <th><a href="{{ sort_uri(request(), 'id')}}">id</a></th>
+        @if (count($paginator) > 0)
+        <div class="panel panel-default">
+          <div class="panel-body">
+            <table class="table table-striped task-table">
+              <tbody>
+              <thead>
+              <tr>
+                <th class="text-center"><a href="{{ sort_uri(request(), 'id')}}">#</a></th>
                 <th><a href="{{ sort_uri(request(), 'name')}}">name</a></th>
                 <th><a href="{{ sort_uri(request(), 'email')}}">email</a></th>
                 <th><a href="{{ sort_uri(request(), 'created_at')}}">created_at</a></th>
                 <th><a href="{{ sort_uri(request(), 'updated_at')}}">updated_at</a></th>
-                </thead>
-                <tbody>
-                @foreach ($paginator as $user)
-                  <tr>
-                    <td class="table-text"><div>{{ $user->id }}</div></td>
-                    <td class="table-text"><div>{{ $user->name }}</div></td>
-                    <td class="table-text"><div>{{ $user->email }}</div></td>
-                    <td class="table-text"><div>{{ $user->created_at }}</div></td>
-                    <td class="table-text"><div>{{ $user->updated_at }}</div></td>
-                  </tr>
-                @endforeach
-                </tbody>
-              </table>
-            </div>
+                <th class="text-right">OPTIONS</th>
+              </tr>
+              </thead>
+              @foreach ($paginator as $user)
+                <tr>
+                  <td class="table-text text-center">{{ $user->id }}</td>
+                  <td class="table-text">{{ $user->name }}</td>
+                  <td class="table-text">{{ $user->email }}</td>
+                  <td class="table-text">{{ $user->created_at }}</td>
+                  <td class="table-text">{{ $user->updated_at }}</td>
+                  <td class="text-right">
+                    <a class="btn btn-sm btn-primary" onclick="alert('not yet implemented.')">
+                      <i class="fas fa-eye"></i> View
+                    </a>
+                    <a class="btn btn-sm btn-warning" onclick="alert('not yet implemented.')">
+                      <i class="fas fa-edit"></i> Edit
+                    </a>
+                    <a class="btn btn-sm btn-danger" onclick="alert('not yet implemented.')">
+                      <i class="fas fa-edit"></i> Delete
+                    </a>
+                  </td>
+                </tr>
+              @endforeach
+              </tbody>
+            </table>
           </div>
-          <div>
-            {{ $paginator->links() }}
-          </div>
-        @endif
         </div>
+        <div>
+          {{ $paginator->links() }}
+        </div>
+      @endif
       </div>
     </div>
   </div>
